af11d1f148758dda36ad4fb618b37eb4afd39ed8
Bruno Barreira <brunobarreirasilva@gmail.com>
2018-11-06 04:44:22 -0200

    Removi meu arquivo pessoal de configuracao

--- classes/Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DEBUG', true);
-
-define('DB_DRIVER', 'mysql');
-define('DB_HOSTNAME', '127.0.0.1');
-define('DB_DATABASE', 'loginTeste');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
